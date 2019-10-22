1e7340593f3ec4e28345986042fac877ec9161a3
Logon Aluno <logonaluno@local.com>
2019-10-22 20:36:06 -0300

    Escavador API

--- resources/Escavador.py
+++ resources/Escavador.py
@@ -20,10 +20,41 @@ class Escavador(Resource):
     }
 
     def post(self):
-        response = requests.request('POST', self.URL, headers=self.HEADERS, json=self.PAYLOAD, verify=False)
-        if response.status_code:
-            response = response.json()
-            access_token = response['access_token']
-            expires_in = response['expires_in']
-            refresh_token = response['refresh_token']
-            print(access_token)
+        if(not util.checkAuthorization(request.headers)):
+            return {"message": "ERROR: Chave de acesso inválida"}, 401
+        else:    
+            response = requests.request('POST', self.URL, headers=self.HEADERS,
+            json=self.PAYLOAD, verify=False)
+            if response.status_code:
+                response = response.json()
+                access_token = response['access_token']
+                expires_in = response['expires_in']
+                refresh_token = response['refresh_token']
+                try:
+                    params = request.get_json()
+                except Exception as e:
+                    return {"message":"Parâmetro inválido"},400
+                if params:
+                    if 'term' in params:
+                        return self.search(access_token, params['term'])
+                    else:
+                        return {"message":"Parâmetro inválido"}, 400
+                else:
+                    return {"message":"Parâmetros obrigatórios não enviados"}, 400
+
+    def search(self, access_token, search_term):
+        url = 'https://www.escavador.com/api/v1/busca'
+
+        params = {
+            'q': search_term,  
+            'qo': 't',  
+            'page': '1'  # opcional
+        }
+
+        headers = {
+            'Authorization': "Bearer {}".format(access_token),
+            'X-Requested-With': 'XMLHttpRequest'
+        }
+
+        response = requests.request('GET', url, headers=headers, params=params, verify=False)
+        return response.json()['items']
