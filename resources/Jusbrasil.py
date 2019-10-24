368868b1e9a388aca4569aa10e91f21823de2bc2
Rodrigo Pereira <rodrigo.lebrao@gmail.com>
2019-10-23 21:53:04 -0300

    Jusbrasil

--- resources/Jusbrasil.py
+++ resources/Jusbrasil.py
@@ -25,11 +25,19 @@ class Jusbrasil(Resource):
 
     def do_crawler(self, **kargs):
         page_result = self.getBsObject(self.URL_TARGET + self.PARAM + kargs.get('nome'))
-        print(page_result.find_all('div',{'class':'DocumentSnippet'}))
+        title_divs = page_result.find_all('div',{'class':'DocumentSnippet'})
+        json_response = {}
+        for key, i in enumerate(title_divs):
+            json_response[key] = {"Diário":i.find("a").getText(), "url":i.find("a").get("href")}
+            key = key + 1
+            # print(i.find_all('h2',{"class":"BaseSnippetWrapper-title"}))
+        return json_response
 
     def post(self):
         if(not util.checkAuthorization(request.headers)):
             return {"message": "ERROR: Chave de acesso inválida"}, 401
         else:
             params = request.get_json()
-            self.do_crawler(nome=params['nome'])
+            json_response = {}
+            json_response['citacoes_encontrados'] = self.do_crawler(nome=params['nome_completo'])
+            return json_response
